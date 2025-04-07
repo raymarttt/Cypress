20ad25b71e87af546c26923a9c2efb298ccd177c
Raymart <raymartlachica299@gmail.com>
2025-04-07 16:34:08 +0800

    test:created cypress automation for login test suite

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,22 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+    cy.contains('Swag Labs').should('be.visible')
+    cy.url().should ("include" , '/inventory')
+  })
+  it('user unable to login with invalid username and password',() => {
+  cy.visit('https://www.saucedemo.com/')
+  cy.get('[data-test="username"]').type("standard_userabc")
+  cy.get('[data-test="password"]').type("abc")
+  cy.get('[data-test="login-button"]').click()
+  //cy.get('[data-test="username"]').should('visible')
+  //cy.get('[data-test="password"]').should('be.visible')
+  //cy.get('[data-test="username"]').should('be.visible')
+  cy.get('[data-test="error"]').should('be.visible')
+
+})
+
+})
