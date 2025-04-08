1c1c11e4e0defafff343b4fb572d708c552b46d8
Raymart <raymartlachica299@gmail.com>
2025-04-08 10:38:28 +0800

    Cypress update config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
+//npx cypress run --record --key 20b677ff-4a99-4d3a-9cf8-bf42274799e4
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+module.exports = defineConfig({ 
+  projectId: "42kcwi",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
